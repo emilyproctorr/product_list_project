e04b33efa296e7a80ba349d06389d572e36ab3f8
Emily Proctor <emilyproctorr@gmail.com>
2024-09-23 19:43:41 -0500

    positioning, add to cart button, your cart card

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -5,25 +5,39 @@ import data from '../data.json';
 function Home() {
     return (
       <div>
-        
-        <div className="Page-Title-Container">
-          <h1 className='Page-Title'>Desserts</h1>
-        </div>
+        <div className="outermost-wrapper-container">
 
-        <div className="Image-Grid">
-                {data.map((item, index) => (
-                    <div key={index}>
-                        <img src={item.image.desktop} alt={item.name} />
-                        <div className='text-container'>
-                          <p className='item_category'>{item.category}</p>
-                          <p className='item_name'>{item.name}</p>
-                          <p className='item_price'>${item.price.toFixed(2)}</p>
-                        </div>
-                    </div>
-                ))}
+          <div className="title-images-container">
+            
+            <div className="page-title-container">
+              <h1 className='page-title'>Desserts</h1>
             </div>
 
+              
+            <div className="image-grid-container">
+                  {data.map((item, index) => (
+                      <div key={index} className="image-text-container">
+                          <img src={item.image.desktop} alt={item.name} className='food-image'/>
+                          <button className='add-to-cart-button'>Add to Cart</button>
+                          <div className='text_container'>
+                            <p className='item_category'>{item.category}</p>
+                            <p className='item_name'>{item.name}</p>
+                            <p className='item_price'>${item.price.toFixed(2)}</p>
+                          </div>
+                          
+                      </div>
+                  ))}
+            </div>
 
+          </div>
+          
+        <div className="your-cart-container">
+              <div className="your-cart-card">
+                <h1 className='your-cart-title'>Your Cart</h1>
+              </div>
+        </div>
+            
+        </div>
       </div>
     );
   }
