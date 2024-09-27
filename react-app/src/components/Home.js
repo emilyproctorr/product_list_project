0b41ceddf6a01aac6e5a47651a238a5436d9c2ee
Emily Proctor <emilyproctorr@gmail.com>
2024-09-27 12:09:23 -0500

    final adjustments and comments

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import "./Home.css";
 import data from '../data.json';;
 
@@ -16,6 +16,7 @@ function Home() {
       });
     };
 
+    // increment item quantity from cart
     const handleIncrementItem = (item) => {
       setCart((prevCart) => {
         // map() function -> creates new array with results of calling function on every element of previous array
@@ -31,6 +32,7 @@ function Home() {
       });
     }
 
+    // decrement item quantity from cart
     const handleDecrementItem = (item) => {
       setCart((prevCart) => {
         return prevCart.reduce((newCart, cartItem) => {
@@ -58,15 +60,18 @@ function Home() {
       return cart.reduce((total, cartItem) => total + cartItem.quantity, 0);
     };
 
+    // check if item is in cart
     const isItemInCart = (item) => {
       // returns true if at least one condition returned true
       return cart.some(cartItem => cartItem.name === item.name);
     };
 
+    // get total order price for all items in cart
     const getOrderTotal = () => {
       return cart.reduce((total, cartItem) => total + cartItem.total_price, 0);
     }
 
+    // remove all instances of one item from cart
     const removeItem = (item) => {
       setCart((prevCart) => {
         return prevCart.reduce((newCart, cartItem) => {
@@ -79,12 +84,20 @@ function Home() {
       });
     }
 
+    // open and close popup
     const togglePopup = () => {
       setShowConfirmOrderPopup(!showConfirmOrderPopup);
     };
 
+    // close popup and clear cart when start new order button is clicked
+    const startNewOrderClick = () => {
+      togglePopup();
+      setCart([]);
+    }
+
     return (
       <div>
+        {/* contains everything found in main page */}
         <div className="outermost-wrapper-container">
 
           <div className="title-images-container">
@@ -93,21 +106,25 @@ function Home() {
               <h1 className='page-title'>Desserts</h1>
             </div>
 
-              
+            {/* grid that food images sit in along with all associated information */}
             <div className="image-grid-container">
                   {data.map((item, index) => (
                       <div key={index} className="image-text-container">
                           <div className='image-button-container'>
                             <img src={item.image.desktop} alt={item.name} className={`food-image ${isItemInCart(item) ? 'selected-item-border' : ''}`}/>
 
+                            {/* if item is not found in cart, display particular add to cart button associated with this case */}
                             { !isItemInCart(item) ? (
                                 <button className='add-to-cart-button-no-item' onClick={() => handleAddToCart(item)}>
                                   <img className="add-to-cart-icon" src="/assets/images/icon-add-to-cart.svg" alt="Add to Cart" /> Add to Cart
                                 </button>
+                            
+                            // if item is found in cart, display increment and decrement options for item
                             ) : (
 
                               <div className='edit-cart-items'>
 
+                                {/* decrement item from cart */}
                                 <button className='decrement-button' onClick={() => handleDecrementItem(item)}>
                                   <svg className="decrement-quantity-icon" xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="currentColor" viewBox="0 0 10 2">
                                     <path fill="currentColor" d="M0 .375h10v1.25H0V.375Z"/>
@@ -117,6 +134,7 @@ function Home() {
                                 {/* quantity value is associated with cart, not data file, so have to use cart to find quantity value */}
                                 <p className='item-quantity-button-text'>{cart.find(cartItem => cartItem.name === item.name).quantity}</p>
 
+                                {/* increment item from cart */}
                                 <button className='increment-button' onClick={() => handleIncrementItem(item)}>
                                   <svg className="increment-quantity-icon" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" viewBox="0 0 10 10">
                                     <path fill="currentColor" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/>
@@ -129,6 +147,8 @@ function Home() {
 
                             
                           </div>
+
+                          {/* all information associated with food item */}
                           <div className='text_container'>
                             <p className='item_category'>{item.category}</p>
                             <p className='item_name'>{item.name}</p>
@@ -141,24 +161,35 @@ function Home() {
 
           </div>
           
+          {/* your cart card that displays all items and associated information found in cart, user also has option to confirm current order */}
           <div className="your-cart-container">
               <div className="your-cart-card-container">
+
                 <div className='your-cart-title-container' >
                   <h1 className='your-cart-title'>Your Cart ({getTotalItemsInCart()})</h1>
                 </div>
+                
+                {/* display items and assocaited quantities and prices */}
                 <div className='your-cart-card-items-container'>
+                  {/* if no items are in cart */}
                   {cart.length === 0 ? (
                     <>
                       <img className="empty-cart-icon" src="/assets/images/illustration-empty-cart.svg" alt="Empty Cart Icon" />
                       <h2 className='empty-cart-text'>Your added items will appear here</h2>
                     </>
+
+                  // if at least one item is found in cart
                   ) : (
+
                     <div className="cart-items-container">
 
+                      {/* for each item display food item information along with remove item button */}
                       {cart.map((cartItem, index) => (
                         <div key={index} className="cart-item">
                           <div className='cart-item-info-remove-item-container'>
+
                             <div>
+                              {/* food item information including name, quantity, price, and total price */}
                               <p className='cart-item-name'>{cartItem.name}</p>
                               <div className='quantity-price-container'>
                                 <div className='cart-item-quantity'>{cartItem.quantity}x</div>
@@ -166,6 +197,8 @@ function Home() {
                                 <div className='cart-item-total-price'>${cartItem.total_price.toFixed(2)}</div>
                               </div>
                             </div>
+
+                            {/* remove item button, if clicked all instances/quantities of this item are removed from cart */}
                             <div className='remove-item-button-container'>
                               <button className='remove-item-button' onClick={() => removeItem(cartItem)}>
                                 <svg className="remove-item-icon" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" viewBox="0 0 10 10">
@@ -173,16 +206,19 @@ function Home() {
                                 </svg>
                               </button>
                             </div>
+
                           </div>
                           <hr className="cart-item-divider" />
                         </div>
                       ))}
 
+                      {/* displays total order price */}
                       <div className='order-total-container'>
                         <div className='order-total-title'>Order Total</div> 
                         <div className='order-total-price'>${getOrderTotal().toFixed(2)}</div>
                       </div>
 
+                      {/* carbon neutral image and text */}
                       <div className='carbon-neutral-container'>
                         <svg className="carbon-neutral-icon" xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none" viewBox="0 0 21 20">
                           <path fill="#1EA575" d="M8 18.75H6.125V17.5H8V9.729L5.803 8.41l.644-1.072 2.196 1.318a1.256 1.256 0 0 1 .607 1.072V17.5A1.25 1.25 0 0 1 8 18.75Z"/><path fill="#1EA575" d="M14.25 18.75h-1.875a1.25 1.25 0 0 1-1.25-1.25v-6.875h3.75a2.498 2.498 0 0 0 2.488-2.747 2.594 2.594 0 0 0-2.622-2.253h-.99l-.11-.487C13.283 3.56 11.769 2.5 9.875 2.5a3.762 3.762 0 0 0-3.4 2.179l-.194.417-.54-.072A1.876 1.876 0 0 0 5.5 5a2.5 2.5 0 1 0 0 5v1.25a3.75 3.75 0 0 1 0-7.5h.05a5.019 5.019 0 0 1 4.325-2.5c2.3 0 4.182 1.236 4.845 3.125h.02a3.852 3.852 0 0 1 3.868 3.384 3.75 3.75 0 0 1-3.733 4.116h-2.5V17.5h1.875v1.25Z"/>
@@ -190,22 +226,27 @@ function Home() {
                         <div className='carbon-neutral-text'>This is a <strong>carbon-neutral</strong> delivery</div> 
                       </div>
 
+                      {/* button to confirm order and display popup to start new order */}
                       <button className='confirm-order-button' onClick={togglePopup}>
                         Confirm Order
                       </button>
                     </div>
                   )}
+
                 </div>
 
               </div>
           </div>
 
+          {/* popup shown when user confirms order and given option to start new order */}
+          {/* shows all items ordered and associated item information and total order price */}
           {showConfirmOrderPopup && (
           <div className="popup-container">
             <div className='popup-content'>
 
               <div className='popup-content-container'>
 
+                {/* popup header including icon, title, and additional text */}
                 <div className="popup-title-container">
                   <svg className="order-confirmed-icon" width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M21 32.121L13.5 24.6195L15.6195 22.5L21 27.879L32.3775 16.5L34.5 18.6225L21 32.121Z" fill="#1EA575"/>
@@ -215,14 +256,17 @@ function Home() {
                   <div className="enjoy-food-text">We hope you enjoy your food!</div>
                 </div>
                 
+                {/* displays all items in submitted order including food image and associated item information */}
                 <div className='order-items-container'>
                   {cart.map((cartItem, index) => (
                     <div key={index} className="order-items">
                       <div className='cart-item-info-remove-item-container'>
                         <div className='popup-image-item-info-container'>
+
                           <div>
                             <img src={cartItem.image.desktop} alt={cartItem.name} className="popup-food-image"/>
                           </div>
+
                           <div>
                             <p className='cart-item-name'>{cartItem.name}</p>
                             <div className='quantity-price-container'>
@@ -230,21 +274,26 @@ function Home() {
                               <div className='cart-item-price'>@ ${cartItem.price.toFixed(2)}</div>
                             </div>
                           </div>
+
                         </div>
-                        <div className='order-total-price-container'>
+                        <div className='order-item-total-price-container'>
                           <div className='order-item-total-price'>${cartItem.total_price.toFixed(2)}</div>
                         </div>
+
                       </div>
                       <hr className="cart-item-divider" />
                     </div>
                   ))}
+                  
+                  {/* displays total price of order */}
                   <div className='popup-order-total-container'>
                     <div className='popup-order-total-title'>Order Total</div> 
                     <div className='popup-order-total-price'>${getOrderTotal().toFixed(2)}</div>
                   </div>
                 </div>
 
-                <button className='start-new-order-button' onClick={togglePopup}>
+                {/* button to start new order, if clicked then close popup and clear cart for new order */}
+                <button className='start-new-order-button' onClick={startNewOrderClick}>
                   Start New Order
                 </button>
 
